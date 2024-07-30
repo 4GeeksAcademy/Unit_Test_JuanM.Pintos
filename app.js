f7d2d26830aa37b0d14f89d1e33a0c7efc4e1e16
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-07-30 10:08:19 +0000

    formulas hechas

--- app.js
+++ app.js
@@ -9,19 +9,17 @@ let oneEuroIs = {
     "USD": 1.07, // us dollar
     "GBP": 0.87, // british pound
     }
-
-function fromDollarToYen(USD){
-return USD * oneEuroIs.JPY;
-}
-
-function fromEuroToDollar(EUR){
+//Euro a Dolar (100 euros son 107 dolares//
+ function fromEuroToDollar(EUR){
     return EUR * oneEuroIs.USD;
+    }
+//Dollar a Yen 100 dolares son 93,45 euros, y 93,45 euros son 14.624 yenes //
+    function fromDollarToYen(USD){
+return (USD/oneEuroIs.USD)*oneEuroIs.JPY
 }
-
+// 1000 yenes son 6,3 euros y a su vez son 5,5 libras esterlina
 function fromYenToPound(JPY){
-    return JPY * oneEuroIs.GBP;
+    return (JPY/oneEuroIs.JPY)*oneEuroIs.GBP
 }
 
 module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
-
-console.log(fromYenToPound(50));
